60950a5c7caf25de8f851b2d27b2025856275f04
Clement <cl3m48@gmail.com>
2020-12-04 00:33:05 +0100

    Page achat

--- leroy-gotchi/src/app/achat/achat.component.ts
+++ leroy-gotchi/src/app/achat/achat.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-achat',
-  templateUrl: './achat.component.html',
-  styleUrls: ['./achat.component.css']
+    selector: 'app-achat',
+    templateUrl: './achat.component.html',
+    styleUrls: ['./achat.component.css']
 })
 export class AchatComponent implements OnInit {
 
-  constructor() { }
+    listItem: string[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    addList(item: string) {
+        this.listItem.push(item);
+        console.log(this.listItem.toString());
+    }
 
 }
